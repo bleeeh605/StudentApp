2a55a75d12bb4f7566b83037f595c2f75d7da299
bleeeh605 <yaardimon@gmail.com>
2025-10-26 16:05:06 +0100

    Create event chooses from actual students. Minor refactoring.

--- payment_manager.py
+++ payment_manager.py
@@ -10,22 +10,37 @@ class PaymentManager():
         self._data_base = data_base
         self._calendar = calendar
     
-    def do_routine(self):
+    def do_routine(self) -> None:
         self._stdscr.clear()  # Clear the screen before redrawing
+
         h, w = self._stdscr.getmaxyx()  # Get the height (h) and width (w) of the terminal window
         x = w//4
         y = h//4
         self._stdscr.addstr(y, x, "Connecting...")
         self._stdscr.refresh()
+
         if not ConnectionChecker.is_internet_connection_present():
-            self._stdscr.addstr(y + 1, x, "Connection could not be established. Check your internet connection if you want to use ")
-            self._stdscr.addstr(y + 2, x, "full program functionality. Press any key to continue...")
-            self._stdscr.getch()
+            self._display_connection_unaveilable(x, y)
             return
         
-        updated_students = []
         self._stdscr.addstr(y, x, "Doing an automated update for students. Please wait...")
         self._stdscr.refresh()
+
+        updated_students = self._automatic_student_update()
+
+        self._stdscr.clear()  # Clear the screen before redrawing
+        update_text_strings = self._create_updated_students_texts(updated_students)
+        self._show_update_text(update_text_strings, x, y)
+
+    def _display_connection_unaveilable(self, x: int, y: int) -> None:
+            self._stdscr.clear()  # Clear the screen before redrawing
+            self._stdscr.addstr(y + 1, x, "Connection could not be established. Check your internet connection if you want to use ")
+            self._stdscr.addstr(y + 2, x, "full program functionality. Press any key to continue...")
+            self._stdscr.refresh()
+            self._stdscr.getch()
+
+    def _automatic_student_update(self) -> list[tuple]:
+        updated_students = []
         # Get information for all students
         student_rows = self._data_base.get_students_info()
         now = datetime.now(ZoneInfo("Europe/Berlin"))
@@ -51,19 +66,28 @@ class PaymentManager():
                         # Otherwise mark lesson as unpaid
                         self._calendar.edit_event(lesson, color_id=str(LessonStatus.UNPAID.value))
                         updated_students.append((name, lesson, "unpaid"))
-            # TODO: Optionally check for lessons which are in the past but have their status as still not paid, mark them as yellow etc
+        return updated_students
+
+    def _create_updated_students_texts(self, updated_students: list[tuple]) -> list[str]:
+        update_text_strings = []
         if updated_students:
-            self._stdscr.clear()  # Clear the screen before redrawing
-            for index, update in enumerate(updated_students):
+            for update in updated_students:
                 lesson_start_date = update[1]['start'].get('dateTime', update[1]['start'].get('date'))
                 lesson_start_date = datetime.fromisoformat(lesson_start_date)
                 update_string = f"{update[0]}'s lesson on {lesson_start_date.strftime('%d.%m.%Y')} ({weekday_to_text(lesson_start_date)}) was marked as {update[2]}."
+                update_text_strings.append(update_string)
+        return update_text_strings
+    
+    def _show_update_text(self, text_strings: list[str], x: int, y: int) -> None:
+        if text_strings:
+            for index, text in enumerate(text_strings):
                 if index == 0:
-                    update_string = "Update: " + update_string
+                    text = "Update: " + text
                 y += index
-                self._stdscr.addstr(y, x, update_string)
+                self._stdscr.addstr(y, x, text)
             self._stdscr.addstr(y+1, x, "Press any key to continue...")
         else:
-            self._stdscr.addstr(y, x, "No automated updates were needed. Press any key to continue...")
+            self._stdscr.addstr(y, x, "No automated updates were needed.")
+            self._stdscr.addstr(y+2, x, "Press any key to continue...")
         self._stdscr.refresh()
         self._stdscr.getch()
